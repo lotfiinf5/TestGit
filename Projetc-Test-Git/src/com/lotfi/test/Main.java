847d2f8908ca1f81155d3be016a95229b47d2be1
lotfiinf5 <lotfiinf5@gmail.com>
2021-08-09 14:50:17 -0400

    add comment ling 4

--- Projetc-Test-Git/src/com/lotfi/test/Main.java
+++ Projetc-Test-Git/src/com/lotfi/test/Main.java
@@ -6,5 +6,6 @@ public class Main {
 		System.out.println("====================>3");
 		System.out.println("====================>3");
 		System.out.println("====================>4");
+		System.out.println("====================>5");
 	}
 }
