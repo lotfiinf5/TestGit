7675c148312454625f9a0594ff7a75fe0cf4973a
lotfiinf5 <lotfiinf5@gmail.com>
2021-08-10 13:55:24 -0400

    fffffffffff

--- Projetc-Test-Git/src/com/lotfi/test/Main.java
+++ Projetc-Test-Git/src/com/lotfi/test/Main.java
@@ -3,14 +3,6 @@ package com.lotfi.test;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("====================>3");
-		System.out.println("  ====================>3");
-		System.out.println("====================>4");
-		System.out.println("====================>5");
-		System.out.println("====================>5");
-                System.out.println("====================>88888");
-		System.out.println("====================>6");
-		System.out.println("====================>6");
-
+		System.out.println("====================>34");
 	}
 }
