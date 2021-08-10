01b406e89a30e87be8bbe853fc3e344016bf8793
lotfi <dinodidine213@gmail.com>
2021-08-10 16:24:13 -0400

    changing line2
    changing line2

--- Projetc-Test-Git/src/com/lotfi/test/Main.java
+++ Projetc-Test-Git/src/com/lotfi/test/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("====================>55");
-		System.out.println("====================>41");
+		System.out.println("====================>55");
 		System.out.println("====================>42");
 		System.out.println("====================>43");
 	}
