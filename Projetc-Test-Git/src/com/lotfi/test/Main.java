478726a02a6f53846f4f86649399a78c7bdc7c69
lotfi <dinodidine213@gmail.com>
2021-08-10 12:17:14 -0400

    Update Main.java
    Update Main.java

--- Projetc-Test-Git/src/com/lotfi/test/Main.java
+++ Projetc-Test-Git/src/com/lotfi/test/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("====================>3");
-		System.out.println("====================>3");
+		System.out.println("  ====================>3");
 		System.out.println("====================>4");
 		System.out.println("====================>5");
 		System.out.println("====================>5");
