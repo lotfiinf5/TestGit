00051250a65ec6d631520a010e163e787414d414
lotfiinf5 <lotfiinf5@gmail.com>
2021-08-10 12:34:32 -0400

    remove lines

--- Projetc-Test-Git/src/com/lotfi/test/Main.java
+++ Projetc-Test-Git/src/com/lotfi/test/Main.java
@@ -11,8 +11,6 @@ public class Main {
 
 		System.out.println("====================>6");
 		System.out.println("====================>6");
-		System.out.println("====================>6");
-		System.out.println("====================>6");
 
 	}
 }
